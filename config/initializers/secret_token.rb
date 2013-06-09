71a0876da81676c88d6b08049520ef7d0ce351ad
mukesh-rk <mukesh@rubykitchen.org>
2013-06-09 21:38:37 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardgame::Application.config.secret_token = '0b578d8985f394ab281eb3a2b490b62c9fcae7a605a6e6aed0e916028230c94315f7c61dfa4249ea3d0818a4b114068324f2a33862563c10074f52a6ef16a53f'
